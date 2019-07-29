31ec43f506f30265ce130f2d2d9838d406f4e7fc
EizoTsuboi <eizo.wakuwaku@gmail.com>
2019-07-29 22:14:00 +0900

    Initial Commit

---
+++ taskapp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp2
+//
+//  Created by 坪井衛三 on 2019/07/29.
+//  Copyright © 2019 Eizo Tsuboi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
