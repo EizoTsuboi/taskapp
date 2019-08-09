d854df586349f38b5c22643eb0269e5b2f84bd9e
EizoTsuboi <eizo.wakuwaku@gmail.com>
2019-08-09 20:15:09 +0900

    taskArrayを修正

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 import RealmSwift
 import UserNotifications
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     let realm = try! Realm()
     var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: false)
+
     
     //画面遷移のときに呼ばれるメソッド
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -31,75 +33,57 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             if allTasks.count != 0{
                 task.id = allTasks.max(ofProperty: "id")! + 1
             }
-            
             inputViewController.task = task
         }
-        
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
+        //searchbarにキャンセルボタン表示
+        searchBar.showsCancelButton = true
     }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tableView.reloadData()
     }
-    
-    
-    //デリゲートメソッドの記載
-    
+    //tableViewにおけるデリゲートメソッドの記載
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return taskArray.count
     }
-    
     // Provide a cell object for each row.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Fetch a cell of the appropriate type.
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
         let task = taskArray[indexPath.row]
         cell.textLabel?.text = task.title
-        
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm"
-        
         let dateString:String = formatter.string(from: task.date)
         cell.detailTextLabel?.text = dateString
-        
         return cell
     }
-    
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
         performSegue(withIdentifier: "cellSegue", sender: nil)
     }
-    
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle{
         return .delete
     }
-    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
         if editingStyle == .delete{
             //削除するタスクを取得する
             let task = self.taskArray[indexPath.row]
-            
             //ローカル通知をキャンセルする
             let center = UNUserNotificationCenter.current()
             center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
-            
             //データベースから削除する
             try! realm.write{
                 self.realm.delete(task)
                 tableView.deleteRows(at: [indexPath], with: .fade)
-                
             }
-            
             //見通知のローカル通知一覧をログ出力
             center.getPendingNotificationRequests{(requests: [UNNotificationRequest])in
                 for request in requests{
@@ -108,10 +92,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     print("------------")
                 }
             }
-            
         }
     }
 
-
+    //SerchBardelegateメソッド
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        taskArray = try! Realm().objects(Task.self).filter("category == searchText").sorted(byKeyPath: "date", ascending: false)
+        
+        tableView.reloadData()
+    }
 }
 
